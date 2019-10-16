3551b0531aff8665d1a602ab3ea22c035b6bc44f
Miirraa <romain.boilleau@gmail.com>
2019-10-16 15:05:31 +0200

    ajout index controller

--- Api/controllers/index.js
+++ Api/controllers/index.js
@@ -4,5 +4,12 @@ module.exports = {
     UtilisateurController: require('./utilisateur.controller'),
     VoterController: require('./voter.controller'),
     SoumettreController: require('./soumettre.controller'),
+    AchatController: require('./achat.controller'),
+    BoitierController: require('./boitier.controller'),
+    BraceletController: require('./bracelet.controller'),
+    CadranController: require('./cadran.controller'),
+    CouleurController: require('./couleur.controller'),
+    ModeleController: require('./modele.controller'),
+    MatiereController: require('./matiere.controller')
 };
 
