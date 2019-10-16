f8b1cab7f2381e966cc629ee5e8f97b1390f2331
eliseall <allirol.elise@gmail.com>
2019-10-16 15:04:20 +0200

    Ajout routes

--- Api/routes/index.js
+++ Api/routes/index.js
@@ -2,10 +2,16 @@
 
 class RouterBuilder {
     build(app) {
+        app.use('/achat', require('./achat.router'));
+        app.use('/boitier', require('./boitier.router'));
+        app.use('/bracelet', require('./bracelet.router'));
+        app.use('/cadran', require('./cadran.router'));
+        app.use('/couleur', require('./couleur.router'));
+        app.use('/matiere', require('./matiere.router'));
+        app.use('/modele', require('./modele.router'));
+        app.use('/soumettre', require('./soumettre.router'));
         app.use('/utilisateur', require('./utilisateur.router'));
         app.use('/voter', require('./voter.router'));
-        app.use('/soumettre', require('./soumettre.router'));
-
     }
 }
 
