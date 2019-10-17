5404bc80a66cca7f3724b6f438a82fdfe0b17f3f
Miirraa <romain.boilleau@gmail.com>
2019-10-17 19:15:40 +0200

    Modif statistics

--- Api/controllers/statistics.controller.js
+++ Api/controllers/statistics.controller.js
@@ -11,31 +11,31 @@ class StatisticsController {
         return Modele.count();
     }
 
+    //SELECT m.Cadran_id, c.Nom, COUNT(Cadran_id) AS NbCadran FROM modele m, cadran c WHERE m.Cadran_id = c.id GROUP BY Cadran_id ORDER BY NbCadran DESC
+
     async getCountCadran(){
-        return Modele.findAll({
-            attributes: {
-                include: [[Sequelize.fn("COUNT", sequelize.col("Cadran_id")), "NbCadran"]],
-                exclude: ['createdAt', 'updatedAt', 'Boitier_id', 'Bracelet_id', 'Nom', 'id']
-            },
-            group: ['Cadran_id'],
-            order: ['NbCadran', 'DESC']
+        let res;
+        await sequelize.query('SELECT m.Cadran_id, c.Nom, COUNT(Cadran_id) AS NbCadran FROM modele m, cadran c WHERE m.Cadran_id = c.id GROUP BY Cadran_id ORDER BY NbCadran DESC',
+            {type: sequelize.QueryTypes.SELECT }
+        ).then(cadran => {
+            res = cadran;
         });
+        return res;
     }
 
     async getCountBoitier(){
-        return Modele.findAll({
-            attributes: {
-                include: [[Sequelize.fn("COUNT", sequelize.col("Boitier_id")), "NbBoitier"]],
-                exclude: ['createdAt', 'updatedAt', 'Cadran_id', 'Bracelet_id', 'Nom', 'id']
-            },
-            group: ['Boitier_id'],
-            order: sequelize.literal('NbBoitier DESC')
+        let res;
+        await sequelize.query('SELECT m.Boitier_id, b.Nom, COUNT(Boitier_id) AS NbBoitier FROM modele m, boitier b WHERE m.Boitier_id = b.id GROUP BY Boitier_id ORDER BY NbBoitier DESC',
+            {type: sequelize.QueryTypes.SELECT }
+        ).then(boitier => {
+            res = boitier;
         });
+        return res;
     }
 
     async getCountCouleur(){
         let res;
-        await sequelize.query('SELECT b.Couleur_id, count(b.id) as NbCouleur FROM modele as a LEFT JOIN bracelet as b ON a.Bracelet_id = b.id GROUP BY b.Couleur_id ORDER BY NbCouleur DESC',
+        await sequelize.query('SELECT b.Couleur_id, c.Nom, count(b.id) as NbCouleur FROM modele m, bracelet b, couleur c WHERE m.Bracelet_id = b.id AND b.Couleur_id = c.id GROUP BY b.Couleur_id ORDER BY NbCouleur DESC',
             {type: sequelize.QueryTypes.SELECT }
         ).then(couleurs => {
             res = couleurs;
@@ -45,10 +45,10 @@ class StatisticsController {
 
     async getCountMatiere(){
         let res;
-        await sequelize.query('SELECT b.Matiere_id, count(b.id) as NbMatiere FROM modele as a LEFT JOIN bracelet as b ON a.Bracelet_id = b.id GROUP BY b.Matiere_id ORDER BY NbMatiere DESC',
+        await sequelize.query('SELECT b.Matiere_id, ma.Nom, count(b.id) as NbMatiere FROM modele m, bracelet b, matiere ma WHERE m.Bracelet_id = b.id AND b.Matiere_id = ma.id GROUP BY b. Matiere_id ORDER BY NbMatiere DESC',
             {type: sequelize.QueryTypes.SELECT }
-        ).then(couleurs => {
-            res = couleurs;
+        ).then(Matiere => {
+            res = Matiere;
         });
         return res;
     }
@@ -57,8 +57,8 @@ class StatisticsController {
         let res;
         await sequelize.query('SELECT m.id as Modele_Id, Count(m.id) as NbVote FROM soumettre s, modele m, voter v WHERE v.soumettre_id = s.id AND s.Modele_id = m.id GROUP BY m.id ORDER BY NbVote DESC',
             {type: sequelize.QueryTypes.SELECT }
-        ).then(couleurs => {
-            res = couleurs;
+        ).then(Vote => {
+            res = Vote;
         });
         return res;
     }
